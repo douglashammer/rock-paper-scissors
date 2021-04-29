e299f8b195cfd394f4b996f68ca92f8d0958bfc8
Doug Hammer <doug.hammer16@gmail.com>
2021-04-29 12:46:35 -0700

    First attempt at js mechanics

---
+++ Desktop/the-odin-project/rock-paper-scissors/script.js
@@ -0,0 +1,12 @@
+// randomly returns rock, paper, or scissors
+function computerPlay() {
+  const moves = ['rock', 'paper', 'scissors'];
+  let move = moves[Math.floor(Math.random() * moves.length)];
+  return move;
+}
+
+// 
+function playRound(playerSelection, computerSelection) {
+
+}
+
