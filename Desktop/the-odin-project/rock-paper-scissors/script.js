72b84449465dfd526de99c70f6359c896acf00f6
Doug Hammer <doug.hammer16@gmail.com>
2021-05-03 13:45:32 -0700

    Changed CSS styling/ almost finished js

--- Desktop/the-odin-project/rock-paper-scissors/script.js
+++ Desktop/the-odin-project/rock-paper-scissors/script.js
@@ -1,83 +1,61 @@
-const gameForm = document.querySelector('form');
-const playerMove = document.querySelector('.user-move').value;
-const playerSelection = playerMove;
+const selectionButtons = document.querySelectorAll('[data-selection]');
+const playButton = document.querySelector('.play-btn');
+
+const moves = [
+	{
+		name: 'rock',
+		emoji: '✊',
+		beats: 'scissors',
+	},
+	{
+		name: 'paper',
+		emoji: '✋',
+		beats: 'rock',
+	},
+	{
+		name: 'scissors',
+		emoji: '✌',
+		beats: 'paper',
+	},
+];
+
+selectionButtons.forEach((selectionButton) => {
+	selectionButton.addEventListener('click', (e) => {
+		const selectionName = selectionButton.dataset.selection;
+		const selection = moves.find((move) => move.name === selectionName);
+
+		playButton.addEventListener('click', (e) => {
+			makeSelection(selection);
+		});
+	});
+});
+
+function makeSelection(selection) {
+	const computerSelection = randomSelection();
+	const youWin = isWinner(selection, computerSelection);
+	const compWins = isWinner(computerSelection, selection);
+
+	if (youWin) {
+    console.log('you win');
+    console.log(selection)
+    console.log(computerSelection);
+	} else if (compWins) {
+    console.log('computer wins');
+    console.log(selection)
+    console.log(computerSelection);
+	} else {
+    console.log('its a draw');
+    console.log(selection)
+    console.log(computerSelection);
+	}
+}
+
+function isWinner(selection, opponentSelection) {
+	return selection.beats === opponentSelection.name;
+}
 
 // randomly returns rock, paper, or scissors
-function computerPlay() {
-	const moves = ['rock', 'paper', 'scissors'];
+function randomSelection() {
 	let move = moves[Math.floor(Math.random() * moves.length)];
 	return move;
 }
-
-// checks for valid move selection
-function checkPlayerSelection(playerSelection) {
-	const regex = /rock|paper|scissors/gi;
-	return regex.test(playerSelection);
-}
-
-// generates user selection against computer selection
-function playRound(playerSelection, computerSelection) {
-	let winner = '';
-
-	if (!checkPlayerSelection(playerSelection)) {
-		return `"${playerSelection}" is not a valid selection.`;
-	} else {
-		if (playerSelection === 'rock' && computerSelection === 'rock') {
-			return "It's a draw!";
-		} else if (playerSelection === 'rock' && computerSelection === 'paper') {
-			return 'You lose.';
-		} else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-			return 'You win!';
-		} else if (playerSelection === 'paper' && computerSelection === 'paper') {
-			return "It's a draw!";
-		} else if (
-			playerSelection === 'paper' &&
-			computerSelection === 'scissors'
-		) {
-			return 'You lose.';
-		} else if (playerSelection === 'paper' && computerSelection === 'rock') {
-			return 'You win!';
-		} else if (
-			playerSelection === 'scissors' &&
-			computerSelection === 'scissors'
-		) {
-			return "It's a draw!";
-		} else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-			return 'You lose!';
-		} else if (
-			playerSelection === 'scissors' &&
-			computerSelection === 'paper'
-		) {
-			return 'You win!';
-		}
-	}
-}
-
-function playGame() {
-	const computerSelection = computerPlay();
-	let playerScore = 0;
-	let opponentScore = 0;
-	let rounds = 5;
-
-	for (let i = 0; i < rounds; i++) {
-
-		let round = playRound(playerSelection, computerSelection);
-		if (round === 'You win!') {
-			playerScore += 1;
-		} else if (round === 'You lose!') {
-			opponentScore += 1;
-		}
-	}
-	if (playerScore > opponentScore) {
-		console.log('You win!!');
-	} else {
-		console.log('You Lost!!!');
-	}
-}
-
-gameForm.addEventListener('submit', (e) => {
-	e.preventDefault();
-
-	checkPlayerSelection(playerSelection);
-	playGame();
-});
